9df98bb1b955d098fead97cb109334236401d3b7
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-07-08 23:12:26 +0000

    Adding background particles

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import BackgroundPartiles from './components/BackgroundParticles/BackgroundParticles'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <BackgroundPartiles />
       <Logo />
       <Rank />
       <ImageLinkForm />
